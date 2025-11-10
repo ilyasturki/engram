c01fc6612222a7eec0956221d92e9f45bb6bbba2
Ilyas Turki <turki.ilyass@gmail.com>
2025-11-10 16:30:43 +0100

    feat: readable video in home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const videoSrc = '/api/video/file_example_MP4_1920_18MG.mp4'
+</script>
+
 <template>
-    <div>Welcome to the Engram!</div>
+    <div class="flex min-h-screen items-center justify-center p-4">
+        <!-- eslint-disable-next-line vue-a11y/media-has-caption -->
+        <video
+            :src="videoSrc"
+            controls
+            class="w-full max-w-4xl rounded-lg shadow-lg"
+        >
+            Your browser does not support the video tag.
+        </video>
+    </div>
 </template>
