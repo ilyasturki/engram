a5e10ceca44dbfb91892169742a2d33964cd5771
Ilyas Turki <turki.ilyass@gmail.com>
2025-11-12 05:27:52 +0100

    feat: simple library page that lists folder

---
+++ app/pages/library/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { data: folders, error } = useFetch('/api/folders')
+</script>
+
+<template>
+    <div
+        v-if="error"
+        class="text-center"
+    >
+        <p class="text-red-500">Error loading folders: {{ error }}</p>
+    </div>
+    <ul v-else>
+        <li>
+            <NuxtLink
+                v-for="folder in folders"
+                :key="folder.path"
+                :to="`/library/${folder.path}`"
+            >
+                {{ folder.path }}
+            </NuxtLink>
+        </li>
+    </ul>
+</template>
