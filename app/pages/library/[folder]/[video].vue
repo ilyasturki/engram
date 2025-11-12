eb2374f29887a8eaedcdcff449996c5cc8aefbda
Ilyas Turki <turki.ilyass@gmail.com>
2025-11-12 05:28:01 +0100

    feat: video page

---
+++ app/pages/library/[folder]/[video].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute()
+const folder = route.params.folder as string
+const video = route.params.video as string
+const videoSrc = `/api/video/${folder}/${video}`
+</script>
+
+<template>
+    <div class="flex min-h-screen items-center justify-center p-4">
+        <VideoPlayer :src="videoSrc" />
+    </div>
+</template>
