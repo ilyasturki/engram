6179fa4b0910e2de3c67c8b5e28431a99a88fd00
Ilyas Turki <turki.ilyass@gmail.com>
2025-11-12 06:29:19 +0100

    feat: back to parent component in video player

--- app/pages/library/[folder]/[video].vue
+++ app/pages/library/[folder]/[video].vue
@@ -6,7 +6,16 @@ const videoSrc = `/api/video/${folder}/${video}`
 </script>
 
 <template>
-    <div class="flex min-h-screen items-center justify-center p-4">
-        <VideoPlayer :src="videoSrc" />
+    <div class="min-h-screen p-4">
+        <NuxtLink
+            :to="`/library/${folder}`"
+            class="mb-6 inline-flex items-center gap-2 text-gray-600 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
+        >
+            <Icon name="lucide:arrow-left" class="size-5" />
+            <span>Back to folder</span>
+        </NuxtLink>
+        <div class="flex min-h-[calc(100vh-8rem)] items-center justify-center">
+            <VideoPlayer :src="videoSrc" />
+        </div>
     </div>
 </template>
